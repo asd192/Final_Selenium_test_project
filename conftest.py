858271bdc90aa47f973d030efe6baf4ba0c5ffc5
Dmitry <PanshinDA@yandex.ru>
2020-10-02 00:42:44 +0500

    added: base_page.py, main_page.py modified: conftest.py, test_main_page.py

--- conftest.py
+++ conftest.py
@@ -6,8 +6,8 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
-                     help="Choose language: es fr ru")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language: en ru es fr...")
 
 
 @pytest.fixture(scope="function")
