302674f46a0bf2112c1d111c9ce76191f325b857
Dmitry <PanshinDA@yandex.ru>
2020-10-04 00:39:30 +0500

    Minor edits. Added pytest.ini.

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 # pytest -v --tb=line --language=en test_main_page.py
+# -m login_guest
 import pytest
 
 from .pages.main_page import MainPage
@@ -8,18 +9,27 @@ from .pages.basket_page import BasketPage
 link = "http://selenium1py.pythonanywhere.com/"
 
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
-
-
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
 
 def test_should_be_login_page(browser):
